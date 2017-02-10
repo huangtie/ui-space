3ab124f07f8685e1e046d1227df0f05628278445
huangtie <huangtie@360.cn>
2017-02-10 19:00:41 +0800

    1

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -9,7 +9,7 @@
 
 return array(
     'DB_TYPE'=>'mysql',//数据库类型
-    'DB_HOST'=>'59.110.46.56',//数据库服务器地址
+    'DB_HOST'=>'127.0.0.1',//数据库服务器地址
     'DB_NAME'=>'uispace', //数据库名
     'DB_USER'=>'root', //数据库用户名
     'DB_PWD'=>'5214021',//数据库用户密码
